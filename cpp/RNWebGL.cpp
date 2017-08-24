c9bc7604fe7c0bbd910e499f5ed607f93448e23c
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-08-24 14:41:53 +0200

    remove dead code and upgrade example RN

--- cpp/RNWebGL.cpp
+++ cpp/RNWebGL.cpp
@@ -884,40 +884,6 @@ private:
 
   _WRAP_METHOD_IS_OBJECT(Texture)
 
-  inline void decodeURI(char *dst, const char *src) {
-    char a, b;
-    while (*src) {
-      if ((*src == '%') &&
-          ((a = src[1]) && (b = src[2])) &&
-          (isxdigit(a) && isxdigit(b))) {
-        if (a >= 'a') {
-          a -= 'a' - 'A';
-        }
-        if (a >= 'A') {
-          a -= ('A' - 10);
-        } else {
-          a -= '0';
-        }
-        if (b >= 'a') {
-          b -= 'a' - 'A';
-        }
-        if (b >= 'A') {
-          b -= ('A' - 10);
-        } else {
-          b -= '0';
-        }
-        *dst++ = 16 * a + b;
-        src += 3;
-      } else if (*src == '+') {
-        *dst++ = ' ';
-        src++;
-      } else {
-        *dst++ = *src++;
-      }
-    }
-    *dst++ = '\0';
-  }
-
   _WRAP_METHOD(texImage2D, 6) {
     if (jsArgc == 9) {
       // 9-argument version
