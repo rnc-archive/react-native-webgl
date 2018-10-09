1a6f84280c26f184e335a6cf6989eaf996c82021
Michał Mokijewski <hello@naf.gl>
2018-10-09 16:51:04 +0200

    use libc++ instead of gnustl
    gnu stl was removed in Android NDK 18

--- android/src/main/jni/Application.mk
+++ android/src/main/jni/Application.mk
@@ -1,13 +1,13 @@
 APP_BUILD_SCRIPT := Android.mk
 
 APP_ABI := armeabi-v7a x86
-APP_PLATFORM := android-9
+APP_PLATFORM := android-16
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
 NDK_MODULE_PATH:=.$(HOST_DIRSEP)$(THIRD_PARTY_NDK_DIR)
 
-APP_STL := gnustl_shared
+APP_STL := c++_shared
 APP_CPPFLAGS := -std=c++11 -fexceptions -pthread
 
 # Make sure every shared lib includes a .note.gnu.build-id header
@@ -16,5 +16,3 @@ APP_LDFLAGS += -llog
 APP_LDFLAGS += -lGLESv2
 APP_LDFLAGS += -pthread
 APP_LDFLAGS += -ljnigraphics
-
-NDK_TOOLCHAIN_VERSION := 4.9
